d3420e71f95f5a87a87b61f7bf7d29c81c41d5f6
jesuscrackflag191 <jesusmolinarodrigue26@gmail.com>
2022-06-08 17:53:55 -0300

    Absoluto 🥵

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="description" content="A boy who has nothing to do" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p className="top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">Hello World!</p>
+      <p className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">Hello World!</p>
     </div>
   )
 }
